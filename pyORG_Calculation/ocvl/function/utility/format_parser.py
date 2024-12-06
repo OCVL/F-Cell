5e9b6d56b0b886efb0aa6fb4ddcce429cff4def7
Robert F Cooper <robert.cooper@marquette.edu>
2024-12-06 14:48:07 -0600

    Fix for missing parser info (e.g. if we don't have mask file)

--- pyORG_Calculation/ocvl/function/utility/format_parser.py
+++ pyORG_Calculation/ocvl/function/utility/format_parser.py
@@ -7,12 +7,21 @@ class FormatParser():
     def __init__(self, video_format, mask_format=None, image_format=None, metadata_format=None):
         self.formatlocs = dict()
         self.vid_parser = parse.compile(video_format)
-        if mask_format:
+
+        if mask_format is not None:
             self.mask_parser = parse.compile(mask_format)
-        if image_format:
+        else:
+            self.mask_parser = None
+
+        if image_format is not None:
             self.im_parser = parse.compile(image_format)
-        if metadata_format:
+        else:
+            self.im_parser = None
+
+        if metadata_format is not None:
             self.metadata_parser = parse.compile(metadata_format)
+        else:
+            self.metadata_parser = None
 
 
     def parse_file(self, file_string):
@@ -21,16 +30,16 @@ class FormatParser():
 
         parsed_str = self.vid_parser.parse(file_string)
         parser_used = FormatTypes.VIDEO
-        if not parsed_str:
+        if parsed_str is None and self.mask_parser is not None:
             parsed_str = self.mask_parser.parse(file_string)
             parser_used = FormatTypes.MASK
-        if not parsed_str:
+        if parsed_str is None and self.im_parser is not None:
             parsed_str = self.im_parser.parse(file_string)
             parser_used = FormatTypes.IMAGE
-        if not parsed_str:
+        if parsed_str is None and self.metadata_parser is not None:
             parsed_str = self.metadata_parser.parse(file_string)
             parser_used = FormatTypes.METADATA
-        if not parsed_str:
+        if parsed_str is None:
             return None, filename_metadata
 
 
