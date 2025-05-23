Fix for flat-fielding, changed the order of video normalization.

ocvl/function/iORG_summary_and_analysis.py
@@ -14,11 +14,13 @@ from matplotlib import pyplot as plt
 import matplotlib as mpl
 from datetime import datetime
 
+from scipy.ndimage import gaussian_filter
+
 from ocvl.function.analysis.iORG_signal_extraction import extract_n_refine_iorg_signals
 from ocvl.function.analysis.iORG_profile_analyses import summarize_iORG_signals, iORG_signal_metrics
 from ocvl.function.display.iORG_data_display import display_iORG_pop_summary, display_iORG_pop_summary_seq, \
     display_iORG_summary_histogram, display_iORG_summary_overlay, display_iORGs
-from ocvl.function.preprocessing.improc import norm_video
+from ocvl.function.preprocessing.improc import norm_video, flat_field
 from ocvl.function.utility.dataset import parse_file_metadata, initialize_and_load_dataset, Stages
 from ocvl.function.utility.json_format_constants import PreAnalysisPipeline, MetaTags, DataFormatType, DataTags, \
     AcquisiTags, \
@@ -265,9 +267,16 @@ if __name__ == "__main__":
                         dataset = initialize_and_load_dataset(group_datasets.loc[slice_of_life], metadata_params, stage=Stages.ANALYSIS)
 
                         if dataset is not None:
-                            # Normalize the video to reduce the influence of framewide intensity changes
-                            dataset.video_data = norm_video(dataset.video_data, norm_method=norm_method, rescaled=rescale_norm,
-                                                            rescale_mean=res_mean, rescale_std=res_stddev)
+                            # Flat field the video for analysis if desired.
+                            if analysis_params.get(PreAnalysisPipeline.FLAT_FIELD, False):
+                                dataset.video_data = flat_field(dataset.video_data, dataset.mask_data)
+
+                            # Gaussian blur the data first before analysis, if requested
+                            gausblur = analysis_params.get(PreAnalysisPipeline.GAUSSIAN_BLUR, 0.0)
+                            if gausblur is not None and gausblur != 0.0:
+                                for f in range(dataset.video_data.shape[-1]):
+                                    dataset.video_data[..., f] = gaussian_filter(dataset.video_data[..., f], sigma=gausblur)
+
 
                             if output_norm_vid:
                                 if analysis_params.get(Analysis.OUTPUT_SUBFOLDER, True):
@@ -278,6 +287,11 @@ if __name__ == "__main__":
                                     result_folder.mkdir(parents=True, exist_ok=True)
 
                                 save_tiff_stack(result_folder.joinpath(dataset.video_path.stem+"_"+norm_method+"_norm.tif"), dataset.video_data)
+
+                            # Normalize the video to reduce the influence of framewide intensity changes
+                            dataset.video_data = norm_video(dataset.video_data, norm_method=norm_method,
+                                                                rescaled=rescale_norm,
+                                                                rescale_mean=res_mean, rescale_std=res_stddev)
 
                             group_datasets.loc[slice_of_life & only_vids, AcquisiTags.DATASET] = dataset
 
@@ -987,7 +1001,7 @@ if __name__ == "__main__":
                                                                                          stim_datasets[0].avg_image_data.shape[1],
                                                                                          -1), copy=True)
 
-                                video_profiles[np.isnan(video_profiles)] = 0
+                                video_profiles[np.isnan(video_profiles)] = starting
                                 save_video(result_folder.joinpath("pooled_pixelpop_iORG_" + now_timestamp + ".avi"),
                                            video_profiles, pooled_framerate.item(),
                                            scalar_mapper=mapper)

ocvl/function/preprocessing/improc.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import SimpleITK as sitk
 from matplotlib import pyplot, pyplot as plt
+from scipy import ndimage
 from scipy.fft import next_fast_len, fft2, ifft2
 from scipy.ndimage import binary_erosion
 from scipy.signal import fftconvolve
@@ -12,39 +13,52 @@ from numpy.polynomial import Polynomial
 from ocvl.function.utility.resources import save_video, save_tiff_stack
 
 
-def flat_field_frame(dataframe, sigma, rescale=False):
+def flat_field_frame(dataframe, mask=None, sigma=31, rescale=False):
     kernelsize = 3 * sigma
     if (kernelsize % 2) == 0:
         kernelsize += 1
 
-    mask = np.ones(dataframe.shape, dtype=dataframe.dtype)
-    mask[dataframe == 0] = 0
+    dataframe = dataframe.astype("float32")
+    minval = np.nanmin(dataframe)
+    maxval = np.nanmax(dataframe)
 
-    dataframe[dataframe == 0] = 1
-    blurred_frame = cv2.GaussianBlur(dataframe.astype("float32"), (kernelsize, kernelsize),
+    if mask is None:
+        mask = np.ones(dataframe.shape, dtype=dataframe.dtype)
+        mask[dataframe == 0] = 0
+    else:
+        mask = mask.astype("float32")
+
+    mask = ndimage.binary_closing(mask, np.ones((5,5))).astype("float32")
+
+    #dataframe[dataframe == 0] = 0
+    dataframe[np.isnan(dataframe)] = 0
+
+    dataframe *= mask
+
+    blurred_frame = cv2.GaussianBlur(dataframe, (kernelsize, kernelsize),
                                      sigmaX=sigma, sigmaY=sigma)
-    flat_fielded = (dataframe.astype("float32") / blurred_frame)
+    blurred_mask = cv2.GaussianBlur(mask, (kernelsize, kernelsize),
+                                     sigmaX=sigma, sigmaY=sigma)
+    blurred_frame /= blurred_mask
 
+    blurred_frame /= np.nanmean(blurred_frame[:])
+
+    flat_fielded = (dataframe / blurred_frame)
+    flat_fielded[flat_fielded < minval] = minval
+    flat_fielded[flat_fielded > maxval] = maxval
+
+    mask[mask == 0] = np.nan
     flat_fielded *= mask
-
-    if rescale:
-        flat_fielded -= np.amin(flat_fielded)
-        flat_fielded = np.divide(flat_fielded, np.amax(flat_fielded), where=flat_fielded != 0)
-
-        if dataframe.dtype == "uint8":
-            flat_fielded *= 255
-        elif dataframe.dtype == "uint16":
-            flat_fielded *= 65535
 
     return flat_fielded
 
 
-def flat_field(dataset, sigma=31, rescale=False):
+def flat_field(dataset, mask=None, sigma=31, rescale=True):
 
     if len(dataset.shape) > 2:
         flat_fielded_dataset = np.zeros(dataset.shape)
         for i in range(dataset.shape[-1]):
-            flat_fielded_dataset[..., i] = flat_field_frame(dataset[..., i], sigma, rescale)
+            flat_fielded_dataset[..., i] = flat_field_frame(dataset[..., i].copy(), mask[..., i].copy(), sigma, rescale)
 
         return flat_fielded_dataset
     else: